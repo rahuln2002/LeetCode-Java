b323db2a756ccda6305798938cf404e6dcf19b9d
Rahul Nihalani <rahulnihalani007@gmail.com>
2023-04-09 21:08:50 +0530

    Update 88.) Merge Sorted Array.java

--- 88.) Merge Sorted Array.java
+++ 88.) Merge Sorted Array.java
@@ -1,12 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=0;i<n;i++) {
-            nums1[i+m] = nums2[i];
+        int i = m - 1;     // nums1's index (actual nums)
+        int j = n - 1;     // nums2's index
+        int k = m + n - 1; // nums1's index (next filled position)
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            }
+            else {
+                nums1[k--] = nums2[j--];
+            }
         }
-        Arrays.sort(nums1);
     }
 }
-
-/*Time Complexity: O(N logN)
+/*Time Complexity: O(m+n)
 * Space Complexity: O(1)
 */
