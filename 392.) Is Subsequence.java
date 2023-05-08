f5b22080255967e08d16a07048022c349d57d096
Rahul Nihalani <rahulnihalani007@gmail.com>
2023-05-09 01:17:31 +0530

    Create 392.) Is Subsequence.java

---
+++ 392.) Is Subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.isEmpty())
+      return true;
+
+    int i = 0;
+    for (final char c : t.toCharArray())
+      if (s.charAt(i) == c && ++i == s.length())
+        return true;
+
+    return false;
+    }
+}
+/* Time Complexity: O(n)
+* Space Complexity: O(1)
+*/
