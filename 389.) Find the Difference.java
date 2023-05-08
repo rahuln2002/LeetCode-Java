5986360d55fc4038ad1de8cdc445d5043acf96ea
Rahul Nihalani <rahulnihalani007@gmail.com>
2023-05-09 01:02:47 +0530

    Create 389.) Find the Difference.java

---
+++ 389.) Find the Difference.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char ans = 0;
+        for (final char c : s.toCharArray()) {
+            ans ^= c;
+        }
+        for (final char c : t.toCharArray()) {
+            ans ^= c;
+        }
+        return ans;
+    }
+}
+/* Time Complexity: O(n)
+* Space Complexity: O(1)
+*/
