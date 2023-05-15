30e2a6290709f65ef49b5e67cd8ce86a44661264
Rahul Nihalani <rahulnihalani007@gmail.com>
2023-05-15 22:57:36 +0530

    Create 1721.) Swapping Nodes in a Linked List.java

---
+++ 1721.) Swapping Nodes in a Linked List.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode first = head;
+        for (int i = 1; i < k; i++) {
+            first = first.next;
+        }
+        ListNode second = head;
+        ListNode temp = first;
+        while (temp.next != null) {
+            temp = temp.next;
+            second = second.next;
+        }
+        int tempval = first.val;
+        first.val = second.val;
+        second.val = tempval;
+
+        return head;
+    }
+}
+/* Time Complexity: O(n)
+* Space Complexity: O(1)
+*/
