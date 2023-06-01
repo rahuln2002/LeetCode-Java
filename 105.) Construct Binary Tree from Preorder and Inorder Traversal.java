0a06c1954e299affbd92ae46afca377ec6b2e1e2
Rahul Nihalani <rahulnihalani007@gmail.com>
2023-06-01 21:36:22 +0530

    Create 105.) Construct Binary Tree from Preorder and Inorder Traversal.java

---
+++ 105.) Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null || preorder.length == 0 || inorder.length == 0) {
+            return null;
+        }
+
+        return buildTreeHelper(preorder, inorder, 0, 0, inorder.length - 1);
+    }
+
+    private TreeNode buildTreeHelper(int[] preorder, int[] inorder, int preStart, int inStart, int inEnd) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) {
+            return null;
+        }
+
+        int rootValue = preorder[preStart];
+        TreeNode root = new TreeNode(rootValue);
+
+        int rootIndexInorder = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == rootValue) {
+                rootIndexInorder = i;
+                break;
+            }
+        }
+
+        int leftSubtreeSize = rootIndexInorder - inStart;
+
+        root.left = buildTreeHelper(preorder, inorder, preStart + 1, inStart, rootIndexInorder - 1);
+        root.right = buildTreeHelper(preorder, inorder, preStart + leftSubtreeSize + 1, rootIndexInorder + 1, inEnd);
+
+        return root;
+    }
+}
+/* Time Complexity: O(n)
+* Space Complexity: O(n)
+*/
