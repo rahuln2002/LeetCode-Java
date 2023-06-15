ba84c5ca40fff6603e50e1d2ec3f221ec78243e1
Rahul Nihalani <rahulnihalani007@gmail.com>
2023-06-15 21:43:51 +0530

    Create 347.) Top K Frequent Elements.java

---
+++ 347.) Top K Frequent Elements.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.comparingInt(frequencyMap::get));
+
+        for (int num : frequencyMap.keySet()) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = minHeap.poll();
+        }
+
+        return result;
+    }
+}
+/* Time Complexity: O(n log k)
+* Space Complexity: O(n)
+*/
